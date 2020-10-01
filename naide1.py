66bf4e3d58cda66c4528cabbd0a773e3ee5217cb
Madis <madismaisalumm@gmail.com>
2020-10-01 11:06:09 +0300

    ülesanded teema 3

---
+++ naide1.py
@@ -0,0 +1,8 @@
+kord = 1
+while(kord <= 5):
+    if(kord == 3):
+        kord += 1
+        break
+        continue
+        print(str(kord) + ". tere")
+        kord += 1
---
+++ naide2.py
@@ -0,0 +1,10 @@
+kord = int(input("Sisestage mitu korda äratada:"))
+for k in range(kord)
+    print("Tõuse ja sära!")
+
+
+
+    k=1
+    while(k <= kord):
+        print("Tõuse ja sära!")
+        k += 1
